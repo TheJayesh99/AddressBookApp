3e4e48a3a2c1beb3c704e528647fc3797e711813
TheJayesh99 <jayeshm.official@gmail.com>
2021-08-13 17:18:27 +0530

    Added : Contact class which will have all the fields

--- js/ContactForm.js
+++ js/ContactForm.js
@@ -3,25 +3,48 @@ window.addEventListener("DOMContentLoaded", (event) => {
   const name = document.querySelector("#name");
   const nameError = document.querySelector(".name-error");
   name.addEventListener("input", function () {
-    let nameRegex = RegExp("^[A-Z]{1}[A-z ]{2,}$");
-    if (nameRegex.test(name.value)) {
-      nameError.textContent = "";
-    } else {
-      nameError.textContent = "invalid name";
-    }
+    if (name.value.length == 0) {
+        nameError.textContent = "";
+        return;
+      }
+      try {
+        new Contact().name = name.value;
+        nameError.textContent = "";
+      } catch (error) {
+        nameError.textContent = error;
+      }
   });
 
   //validation for phone number
   const phoneNumber = document.querySelector("#phoneNumber");
   const numberError = document.querySelector(".tel-error");
   phoneNumber.addEventListener("input", function () {
-    let phoneNumberRegex = RegExp("^[0-9]{2}[ ][0-9]{10}$");
-    if (phoneNumberRegex.test(phoneNumber.value)) {
-      console.log(phoneNumber.value);
-      numberError.textContent = "";
-    } else {
-      numberError.textContent = "incorrect number";
-    }
+    if (phoneNumber.value.length == 0) {
+        numberError.textContent = "";
+        return;
+      }
+      try {
+        new Contact().phoneNumber = phoneNumber.value;
+        numberError.textContent = "";
+      } catch (error) {
+        numberError.textContent = error;
+      }
   });
+
+  //validation for zip code
+  const zip = document.querySelector("#zip")
+  const zipError = document.querySelector(".zip-error")
+  zip.addEventListener("input", function () {
+    if (zip.value.length == 0) {
+        zipError.textContent = "";
+        return;
+      }
+      try {
+        new Contact().zip = zip.value;
+        zipError.textContent = "";
+      } catch (error) {
+        zipError.textContent = error;
+      }
+  })
 
 });
